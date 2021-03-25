46e54a979a9f316d0f7e4d4f81c5c7b5abc80d6f
lihogub <oligli@yandex.ru>
2021-03-25 22:02:58 +0000

    gg

--- task3/index.php
+++ task3/index.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: text/html; charset=UTF-8');
-
+ 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   if (!empty($_GET['save'])) {
     print('Спасибо, результаты сохранены.');
