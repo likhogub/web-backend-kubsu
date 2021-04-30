68048d3f9acd686cf22d821df8322fe59c432597
lihogub <oligli@yandex.ru>
2021-04-30 12:06:00 +0000

    added task5

--- task5/form.php
+++ task5/form.php
@@ -1,116 +1,120 @@
+<!DOCTYPE html>
 <html lang="ru">
 <head>
-    <meta charset="UTF-8">
-    <title>TASK 4</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>TASK 5</title>
     <style>
-	   form {
-	       width: 600px;
-	       height: 700px;
-	       background: white;
-	       border-radius: 8px;
-	       margin: 0 auto;
-	       padding: 30px;
-	       box-shadow: 0 0 14px 0 rgba(50, 50, 50, 0.75);
-	   }
-	   p {
-	       line-height: 0.5;
-	   }
-	   label {
-	       margin: 3px;
-	   }
-	   input {
-	       margin: 8px 0;
-	   }
-	   input[type="text"], input[type="email"] {
-	       width: 100%;
-	       height: 30px;
-	       border-radius: 5px;
-	       border: 2px solid grey;
-	       outline: none;
-	       padding: 7px;
-	   }
-	   input[type="checkbox"] {
-	       margin-right: 7px;
-	   }
-	   textarea {
-	       width: 300px;
-	       height: 150px;
-	       padding: 7px;
-	   }
-	   input[type="submit"] {
-	       padding: 7px 20px;
-	       border-radius: 5px;
-	       box-shadow: 0px 0px 5px 0px rgba(46, 53, 55, 0.5);
-	   }
-	   input[type="submit"]:hover {
-	       cursor: pointer;
-	   }
-           .error {
-               border: 1px red solid !important;
-           }
-	</style>
+        .error {
+            border:1px solid red;
+        }
+        .error1 {
+            border:2px solid red;
+            font-size:14pt;
+            padding: 2px;
+            margin: 0 auto 2px;
+            width: 420px;
+            text-align:center;
+        }
+        body {
+            font:24pt sans-serif;
+            text-align:center;
+        }
+        .complete {
+            margin: 0 auto 25px;
+            width: 420px;
+            border:2px solid green;
+            font-size:18pt;
+        }
+        .complete2 {
+            margin: 0 auto 25px;
+            width: 420px;
+            border:2px solid green;
+            font-size:24pt;
+        }
+        form {
+            width: 600px;
+            background: white;
+            border-radius: 8px;
+            margin: 0 auto;
+            padding: 30px;
+            box-shadow: 0 0 14px 0 rgba(50, 50, 50, 0.75);
+            text-align:left;
+            font-size:18pt;
+        }
+        input.submit {
+            width: 128px;
+            height: 32px;
+        }
+    </style>
 </head>
 <body>
-<form action="" method="POST">
-    <label>
-      Имя:<br />
-      <input name="fio" <?php if ($errors['fio']) print('class="error"');?>  value="<?php print($NAME);?>"  />
-    </label><br />
-    <label>
-      Email:<br />
-      <input name="email" <?php if ($errors['email']) print('class="error"');?> 
-        value="<?php print($EMAIL);?>"
-        type="email" />
-    </label><br />
-    <label>
-      Год рождения:<br />
-    <select name="year" >
-      <?php for ($i = 1900; $i < 2021;$i++) {
-        print('<option value="');
-        print($i);
-        if ($YEAR == $i) print('" selected="selected">');
-	else print('">');
-        print($i);
-        print('</option>');}?>
-    </select>
-    </label><br />
-    Пол:<br />
-    <label><input type="radio" <?php if ($SEX == "М") print("checked"); ?>
-      name="sex" value="М" />
-      М</label>
-    <label><input type="radio" <?php if ($SEX == "Ж") print("checked");  ?> 
-      name="sex" value="Ж" />
-      Ж</label><br />
-    Количество конечностей:<br />
-<?php for ($i = 1; $i <= 4; $i++) {
-    print('<label><input type="radio" name="limb"');
-    if ($LIMB == $i) print('checked="checked" value="');
-    else print('value="'); 
-    print($i);
-    print('"/>');
-    print($i);
-    print('</label>');
+<?php
+if (!empty($messages)) {
+    print('<div id="messages">');
+    foreach ($messages as $message) {
+        print($message);
+    }
+    print('</div>');
 }
 ?>
-<br />
+<form action="" method="POST">
     <label>
-        Способности:
+        Name: <br />
+        <input name="name" <?php if ($errors['name']) {print 'class="error"';} ?> value="<?php print $values['name']; ?>" type="text"/>
+    </label><br />
+    <label>
+        Email:<br />
+        <input name="email" <?php if ($errors['email']) {print 'class="error"';} ?> value="<?php print $values['email']; ?>"
+               value="sample@example.com"
+               type="text" />
+    </label><br />
+    <label>
+        Birth year:<br />
+        <select name="year" <?php if ($errors['year']) {print 'class="error"';} ?> value="<?php print $values['year']; ?>">
+            <option value="выбрать...">Choose...</option>
+            <?php for($i = 1900; $i < 2021; $i++) {?>
+                <option <?php if ($values['year']==$i){print 'selected="selected"';} ?> value="<?php print $i; ?>"><?= $i; ?></option>
+            <?php }?>
+        </select>
+    </label><br />
+    Sex:<br />
+    <label><input type="radio"
+                  name="sex" <?php if ($values['sex']==0){print 'checked';} ?> value="0" />
+        M</label>
+    <label><input type="radio" <?php if ($values['sex']==1){print 'checked';} ?>
+                  name="sex" value="1" />
+        F</label><br />
+    Limbs count:<br />
+    <label><input type="radio" <?php if ($values['limb']==0 || $values['limb']==1){print 'checked';} ?>
+                  name="limb" value="1" />
+        1</label>
+    <label><input type="radio" <?php if ($values['limb']==2){print 'checked';} ?>
+                  name="limb" value="2" />
+        2</label>
+    <label><input type="radio" <?php if ($values['limb']==3){print 'checked';} ?>
+                  name="limb" value="3" />
+        3</label>
+    <label><input type="radio" <?php if ($values['limb']==4){print 'checked';} ?>
+                  name="limb" value="4" />
+        4</label>
+    <label><br/>
+        Superpowers:
         <br />
-        <select name="power[]"
-          multiple="multiple">
-          <option value="ab_god" <?php if ($GOD) print('selected');  ?> >Бессмертие</option>
-          <option value="ab_clip" <?php if ($CLIP) print('selected');  ?> >Прохождение сквозь стены</option>
-          <option value="ab_fly" <?php if ($FLY) print('selected');  ?>>Левитация</option>
+        <select name="power[]" <?php if ($errors['power']) {print 'class="error"';} ?>
+                multiple="multiple">
+            <option <?php if (in_array("god",$values['power'])){print 'selected="selected"';} ?> value="god">Immortal</option>
+            <option <?php if (in_array("clip",$values['power'])){print 'selected="selected"';} ?> value="clip">Clip</option>
+            <option <?php if (in_array("fly",$values['power'])){print 'selected="selected"';} ?> value="fly">Levitation</option>
         </select>
     </label><br />
     <label>
-      Биография:<br />
-      <textarea name="bio"><?php print($BIO);?> </textarea>
+        Biography (optional):<br />
+        <textarea class="text" name="bio" placeholder="Your biography" rows=10><?php print $values['bio']; ?></textarea>
     </label><br />
     <label><input type="checkbox"
-      name="check" required/>
-      С контрактом ознакомлен</label><br />
-    <input type="submit" value="Отправить" />
+                  name="check" required />
+        Agree with contract</label><br />
+    <input type="submit" class="submit" value="Send" />
 </form>
 </body>
 </html>
--- task5/index.php
+++ task5/index.php
@@ -1,109 +1,285 @@
 <?php
 header('Content-Type: text/html; charset=UTF-8');
- 
+$ability_data = ['god', 'clip', 'fly'];
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-  if (!empty($_COOKIE['save'])) {
-    setcookie('save', '0', 1);
-    print('Спасибо, результаты сохранены.');
-  }
-  
-  $errors = array();
-  $errors['fio'] = !empty($_COOKIE['fio_error']);
-  $errors['email'] = !empty($_COOKIE['email_error']);
-
-  if ($errors['fio']) {
-    setcookie('fio_error', '', 100000);
-    $_COOKIE['fio'] = "";
-    print('<div class="error">Заполните имя.</div>');
-  }
-  if ($errors['email']) {
-    setcookie('email_error', '', 100000);
-    $_COOKIE['email'] = "";
-    print('<div class="error">Заполните e-mail.</div>');
-  }
-
-  $NAME = empty($_COOKIE['fio']) ? '' : $_COOKIE['fio'];
-  $EMAIL = empty($_COOKIE['email']) ? '' : $_COOKIE['email'];
-  $SEX = empty($_COOKIE['sex']) ? '' : $_COOKIE['sex'];
-  $YEAR = empty($_COOKIE['year']) ? '' : $_COOKIE['year'];
-  $LIMB = empty($_COOKIE['limb']) ? '' : $_COOKIE['limb'];
-  $BIO = empty($_COOKIE['bio']) ? '' : $_COOKIE['bio'];
-  $GOD = empty($_COOKIE['god']) ? '' : $_COOKIE['god'];
-  $CLIP = empty($_COOKIE['clip']) ? '' : $_COOKIE['clip'];
-  $FLY = empty($_COOKIE['fly']) ? '' : $_COOKIE['fly'];
-  include('form.php');
-  exit();
-}
-
-$errors = FALSE;
-if (empty($_POST['fio'])) {
-  setcookie('fio_error', '1', time() + 24 * 60 * 60);
-  $errors = TRUE;
-} else {
-  setcookie('fio', $_POST['fio'], time() + 30 * 24 * 60 * 60);
-}
-
-if (empty($_POST['email'])) {
-  setcookie('email_error', '1', time() + 30 * 24 * 60 * 60);
-  $errors = TRUE;
-
-} else if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-  setcookie('email_error', '1', time() + 30 * 24 * 60 * 60);
-  $errors = TRUE;
-} else {
-  setcookie('email', $_POST['email'], time() + 30 * 24 * 60 * 60);
-}
-
-if ($errors) {
-  header('Location: index.php');
-  exit();
-} else {
-  setcookie('fio_error', '', 100000);
-  setcookie('email_error', '', 100000);
-  setcookie('limb', $_POST['limb'], 0);
-  setcookie('sex', $_POST['sex'], 0);
-  setcookie('year', $_POST['year'], 0);
-  setcookie('god', intval(in_array("ab_god", $_POST['power'])), 0);
-  setcookie('fly', intval(in_array("ab_fly", $_POST['power'])), 0);
-  setcookie('clip', intval(in_array("ab_clip", $_POST['power'])), 0);
-}
-
-setcookie('save', '1', 0);
-header('Location: index.php');
-
-$user = 'task1user';
-$pass = 'task1pass';
-$db = new PDO('mysql:host=localhost;dbname=study4', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
-try {
-    $stmt = $db->prepare("INSERT INTO userbase (name, year, sex, email, bio, limb) VALUES (:name, :year, :sex, :email, :bio, :limb)");
-    $stmt->bindParam(':name', $_POST['fio']);
-    $stmt->bindParam(':year', $_POST['year']);
-    $stmt->bindParam(':sex', $_POST['sex']);
-    $stmt->bindParam(':email', $_POST['email']);
-    $stmt->bindParam(':bio', $_POST['bio']);
-    $stmt->bindParam(':limb', intval($_POST['limb']));
-    $stmt->execute();
-    $last_id = $db->lastInsertId();
-    if (in_array("ab_god", $_POST['power'])) {
-        $stmt = $db->prepare("INSERT INTO usersuperpower (id, power) VALUES (:id, :power)");
-        $stmt->bindParam(':id', $last_id);
-        $stmt->bindParam(':power', intval(0));
-        $stmt->execute();
+    $messages = array();
+    if (!empty($_COOKIE['save'])) {
+        setcookie('save', '', 100000);
+        setcookie('login', '', 100000);
+        setcookie('pass', '', 100000);
+        $messages[] = '<div class="complete">Data saved</div>';
+        if (!empty($_COOKIE['pass'])) {
+            $messages[] = sprintf(
+                '<div class = "complete2">
+                            <a href="login.php">Sign-in</a> <br/>username <strong>%s</strong> <br/>pass <strong>%s</strong></div>',
+                strip_tags($_COOKIE['login']),
+                strip_tags($_COOKIE['pass']));
+        }
     }
-    if (in_array("ab_clip", $_POST['power'])) {
-        $stmt = $db->prepare("INSERT INTO usersuperpower (id, power) VALUES (:id, :power)");
-        $stmt->bindParam(':id', $last_id);
-        $stmt->bindParam(':power', intval(1));
-        $stmt->execute();
+    $errors = array();
+    $errors['name'] = !empty($_COOKIE['name_error']);
+    $errors['email'] = !empty($_COOKIE['email_error']);
+    $errors['year'] = !empty($_COOKIE['year_error']);
+    $errors['power'] = !empty($_COOKIE['power_error']);
+    if ($errors['name']) {
+        setcookie('name_error', '', 100000);
+        $messages[] = '<div class="error1">Fill name</div>';
     }
-    if (in_array("ab_fly", $_POST['power'])) {
-        $stmt = $db->prepare("INSERT INTO usersuperpower (id, power) VALUES (:id, :power)");
-        $stmt->bindParam(':id', $last_id);
-        $stmt->bindParam(':power', intval(2));
-        $stmt->execute();
+    if ($errors['email']) {
+        if ($errors['email'] == 1) {
+            setcookie('email_error', '', 100000);
+            $messages[] = '<div class="error1">Fill valid email</div>';
+        } else {
+            setcookie('email_error', '', 100000);
+            $messages[] = '<div class="error1">Fill valid email</div>';
+        }
     }
-} catch(PDOException $e) {
-    print('Error : ' . $e->getMessage());
-    exit();
-}
+    if ($errors['year']) {
+        setcookie('year_error', '', 100000);
+        $messages[] = '<div class="error1">Choose valid year</div>';
+    }
+    if ($errors['power']) {
+        setcookie('power_error', '', 100000);
+        $messages[] = '<div class="error1">Choose superpower</div>';
+    }
+    $values = array();
+    $values['name'] = empty($_COOKIE['name_value']) ? '' : $_COOKIE['name_value'];
+    $values['email'] = empty($_COOKIE['email_value']) ? '' : $_COOKIE['email_value'];
+    $values['year'] = empty($_COOKIE['year_value']) ? '' : $_COOKIE['year_value'];
+    $values['power'] = empty($_COOKIE['power_value']) ? '' : unserialize($_COOKIE['power_value']);
+    $values['sex'] = empty($_COOKIE['sex_value']) ? '' : $_COOKIE['sex_value'];
+    $values['limb'] = empty($_COOKIE['limb_value']) ? '' : $_COOKIE['limb_value'];
+    $values['bio'] = empty($_COOKIE['bio_value']) ? '' : $_COOKIE['bio_value'];
 
+    if (!isset($_SESSION)) {
+        session_start();
+    }
+
+    if ($errors &&
+        !empty($_COOKIE[session_name()]) && !empty($_SESSION['login'])
+    ) {
+        $user = 'task1user';
+        $pass = 'task1pass';
+        $db = new PDO('mysql:host=localhost;dbname=study4', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+        try {
+            $stmt = $db->prepare("SELECT id FROM userpassword WHERE login=:i");
+            $result = $stmt->execute(array("i" => $_SESSION['login']));
+            $idbd = (current(current($stmt->fetchAll(PDO::FETCH_ASSOC))));
+        } catch (PDOException $e) {
+            print('Error : ' . $e->getMessage());
+            exit();
+        }
+        try {
+            $stmt = $db->prepare("SELECT * FROM userbase WHERE id=:i");
+            $result = $stmt->execute(array("i" => $idbd));
+            $data = current($stmt->fetchAll(PDO::FETCH_ASSOC));
+        } catch (PDOException $e) {
+            print('Error : ' . $e->getMessage());
+            exit();
+        }
+        $values['name'] = filter_var($data['name'], FILTER_SANITIZE_SPECIAL_CHARS);
+        $values['email'] = filter_var($data['email'], FILTER_SANITIZE_SPECIAL_CHARS);
+        $values['year'] = filter_var($data['year'], FILTER_SANITIZE_SPECIAL_CHARS);
+        $values['sex'] = $data['sex'];
+        $values['limb'] = $data['limb'];
+        $values['bio'] = filter_var($data['bio'], FILTER_SANITIZE_SPECIAL_CHARS);
+        $abil = [];
+        foreach ($data['power'] as $a) {
+            $q = 0;
+            if (in_array($a, $data['power'])) {
+                $abil[$q] = $ability_data[$a];
+                $q++;
+            }
+        }
+        $values['power'] = $abil;
+        printf('<div class="complete2"> Logged as %s</div>', $_SESSION['login']);
+    }
+    include('login.php');
+    include('form.php');
+} else {
+    $errors = FALSE;
+
+    if (empty($_POST['name'])) {
+        setcookie('name_error', '1', time() + 24 * 60 * 60);
+        $errors = true;
+    } else {
+        setcookie('name_value', $_POST['name'], time() + 30 * 24 * 60 * 60);
+    }
+
+    if (empty($_POST['email'])) {
+        setcookie('email_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } else if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        setcookie('email_error', '2', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } else {
+        setcookie('email_value', $_POST['email'], time() + 30 * 24 * 60 * 60);
+    }
+
+    if (empty($_POST['year'])) {
+        setcookie('year_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } else {
+        $year = $_POST['year'];
+        if (!(is_numeric($year) && intval($year) >= 1900 && intval($year) < 2020)) {
+            setcookie('year_error', '1', time() + 24 * 60 * 60);
+            $errors = TRUE;
+        } else {
+            setcookie('year_value', $_POST['year'], time() + 30 * 24 * 60 * 60);
+        }
+    }
+
+    if (empty($_POST['power'])) {
+        setcookie('power_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } else {
+        $abilities = $_POST['power'];
+        foreach ($abilities as $ability) {
+            if (!in_array($ability, $ability_data)) {
+                print('Invalid superpower<br>');
+                $errors = TRUE;
+            }
+        }
+    }
+    if ($errors == false) {
+        setcookie('power_value', serialize($_POST['power']), time() + 30 * 24 * 60 * 60);
+        setcookie('sex_value', $_POST['sex'], time() + 30 * 24 * 60 * 60);
+        setcookie('limb_value', $_POST['limb'], time() + 30 * 24 * 60 * 60);
+        setcookie('bio_value', $_POST['bio'], time() + 30 * 24 * 60 * 60);
+    }
+
+    $ability_insert = [];
+    foreach ($ability_data as $ability) {
+        $ability_insert[$ability] = in_array($ability, $abilities) ? 1 : 0;
+    }
+
+    if ($errors) {
+        header('Location: index.php');
+        exit();
+    } else {
+        setcookie('name_error', '', 100000);
+        setcookie('email_error', '', 100000);
+        setcookie('year_error', '', 100000);
+        setcookie('power_error', '', 100000);
+    }
+    if (!isset($_SESSION)) {
+        session_start();
+    }
+    $messages[] = '<div class="complete">SAS</div>';
+    printf($_COOKIE[session_name()]);
+    printf($_SESSION['login']);
+    if (!empty($_COOKIE[session_name()]) && !empty($_SESSION['login'])) {
+        $user = 'task1user';
+        $pass = 'task1pass';
+        $db = new PDO('mysql:host=localhost;dbname=study4', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+        try {
+            $stmt = $db->prepare("SELECT id FROM userpassword WHERE login=:i");
+            $result = $stmt->execute(array("i" => $_SESSION['login']));
+            $idbd = (current(current($stmt->fetchAll(PDO::FETCH_ASSOC))));
+            $stmt1 = $db->prepare("UPDATE userbase SET name=:name, year=:year, sex=:sex, email=:email, bio=:bio, limb=:limb WHERE id =:id");
+            $stmt1->bindParam(':name', $_POST['name']);
+            $stmt1->bindParam(':year', $_POST['year']);
+            $stmt1->bindParam(':sex', $_POST['sex']);
+            $stmt1->bindParam(':email', $_POST['email']);
+            $stmt1->bindParam(':bio', $_POST['bio']);
+            $stmt1->bindParam(':limb', $_POST['limb']);
+            $stmt1->bindParam(':id', $idbd);
+            $stmt1->execute();
+            $stmt0 = $db->prepare("SELECT power FROM usersuperpower WHERE id=:i");
+            $result = $stmt0->execute(array("i" => $idbd));
+            $powerstemp = $stmt0->fetchAll(PDO::FETCH_ASSOC);
+            $powers = [];
+            $q = 0;
+            foreach ($powerstemp as $element) {
+                $powers[$q] = $element[power];
+                $q++;
+            }
+            if (!empty($ability_insert['god']) && !in_array('0', $powers)) {
+                $stmt2 = $db->prepare("INSERT INTO usersuperpower (id, power) VALUES (:id,:power)");
+                $stmt2->bindParam(':id', $idbd);
+                $stmt2->bindParam(':power', intval(0));
+                $stmt2->execute();
+            } else if (empty($ability_insert['god']) && in_array('0', $powers)) {
+                $stmt3 = $db->prepare("DELETE FROM usersuperpower where id=:id and power=:power");
+                $stmt3->bindParam(':id', $idbd);
+                $stmt3->bindParam(':power', intval(0));
+                $stmt3->execute();
+            }
+            if (!empty($ability_insert['clip']) && !in_array('1', $powers)) {
+                $stmt4 = $db->prepare("INSERT INTO usersuperpower (id, power) VALUES (:id,:power)");
+                $stmt4->bindParam(':id', $idbd);
+                $stmt4->bindParam(':power', intval(1));
+                $stmt4->execute();
+            } else if (empty($ability_insert['clip']) && in_array('1', $powers)) {
+                $stmt5 = $db->prepare("DELETE FROM usersuperpower where id=:id and power=:power");
+                $stmt5->bindParam(':id', $idbd);
+                $stmt5->bindParam(':power', intval(1));
+                $stmt5->execute();
+            }
+            if (!empty($ability_insert['fly']) && !in_array('2', $powers)) {
+                $stmt6 = $db->prepare("INSERT INTO usersuperpower (id, power) VALUES (:id,:power)");
+                $stmt6->bindParam(':id', $idbd);
+                $stmt6->bindParam(':power', intval(2));
+                $stmt6->execute();
+            } else if (empty($ability_insert['fly']) && in_array('2', $powers)) {
+                $stmt7 = $db->prepare("DELETE FROM usersuperpower where id=:id and power=:power");
+                $stmt7->bindParam(':id', $idbd);
+                $stmt7->bindParam(':power', intval(2));
+                $stmt7->execute();
+            }
+        } catch (PDOException $e) {
+            print('Error : ' . $e->getMessage());
+            exit();
+        }
+    } else {
+        $login = uniqid("user");
+        $pwd = uniqid("pass");
+        // Сохраняем в Cookies.
+        setcookie('login', $login);
+        setcookie('pass', $pwd);
+
+        $user = 'task1user';
+        $pass = 'task1pass';
+        $db = new PDO('mysql:host=localhost;dbname=study4', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+        try {
+            $stmt = $db->prepare("INSERT INTO userbase (name,year,sex,email,bio,limb) VALUES (:name,:year,:sex,:email,:bio,:limb)");
+            $stmt->bindParam(':name', $_POST['name']);
+            $stmt->bindParam(':year', $_POST['year']);
+            $stmt->bindParam(':sex', $_POST['sex']);
+            $stmt->bindParam(':email', $_POST['email']);
+            $stmt->bindParam(':bio', $_POST['bio']);
+            $stmt->bindParam(':limb', $_POST['limb']);
+            $stmt->execute();
+            $last_id = $db->lastInsertId();
+            if (!empty($ability_insert['god'])) {
+                $stmt1 = $db->prepare("INSERT INTO usersuperpower (id, power) VALUES (:id,:power)");
+                $stmt1->bindParam(':id', intval($last_id));
+                $stmt1->bindParam(':power', intval(0));
+                $stmt1->execute();
+            }
+            if (!empty($ability_insert['clip'])) {
+                $stmt2 = $db->prepare("INSERT INTO usersuperpower (id, power) VALUES (:id,:power)");
+                $stmt2->bindParam(':id', intval($last_id));
+                $stmt2->bindParam(':power', intval(1));
+                $stmt2->execute();
+            }
+            if (!empty($ability_insert['fly'])) {
+                $stmt3 = $db->prepare("INSERT INTO usersuperpower (id, power) VALUES (:id,:power)");
+                $stmt3->bindParam(':id', intval($last_id));
+                $stmt3->bindParam(':power', intval(2));
+                $stmt3->execute();
+            }
+            $stmt4 = $db->prepare("INSERT INTO userpassword (id, login, pwd) VALUES (:id,:login, :pwd)");
+            $stmt4->bindParam(':id', intval($last_id));
+            $stmt4->bindParam(':login', $login);
+            $stmt4->bindParam(':pwd', password_hash($pwd, PASSWORD_DEFAULT));
+            $stmt4->execute();
+        } catch (PDOException $e) {
+            print('Error : ' . $e->getMessage());
+            exit();
+        }
+    }
+    setcookie('save', '1');
+
+    header('Location: index.php');
+}
